0db0249243d084f8aae562bd2130b59332a56fbf
danclark1820 <danclark1820@gmail.com>
2014-04-03 17:45:36 -0400

    correct user sign in story

--- spec/features/user_creates_an_account_spec.rb
+++ spec/features/user_creates_an_account_spec.rb
@@ -3,15 +3,14 @@ require 'spec_helper'
 feature "User adds entry to registry", %q{
   As an un-authenticated user,
   I want to securely register for an account,
-  so that I can rate movies.
+  so I can get ideas for what movies I should watch
 } do
 
 #  Acceptance Criteria
 # *A sign up and authentication system for your users
-# *I must provide a username
 # *I must provide  an unused email
 # *I must provide a password
-# *Provide an option for first name and last name
+
 
   it "can sign up" do
     count = User.count

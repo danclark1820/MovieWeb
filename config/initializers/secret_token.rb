e16383ff6a83b98e30aec03ae89c8ba1a757b2ba
danclark1820 <danclark1820@gmail.com>
2014-04-03 10:45:25 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MovieWeb::Application.config.secret_key_base = '77c21f52eebd9a5b526dcd27c5abe58d98fc5dbc464fd551e806e368fd28ebb548a829206e23e28509feb66ead270b9a8ac48e20b2983db60f16c4c1b0824e33'
---
+++ spec/features/user_creates_an_account_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+feature "User adds entry to registry" %q{
+  As a user,
+  I want to securely register for an account,
+  so that I can rate movies.
+} do
+
+#  Acceptance Criteria
+# *A sign up and authentication system for your users
+# *I must provide a username
+# *I must provide  an unused email
+# *I must provide a password
+# *Provide an option for first name and last name
+
+  it "can sign up" do
+    count = User.count
+    visit new_user_registration_path
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Username', with: 'moviewatcher'
+    fill_in 'user_password', with: 'password'
+    fill_in 'user_password_confirmation', with: 'password'
+    click_on 'Sign Up'
+
+    expect(page).to have_content "Welcome"
+    expect(User.count).to eql(count + 1)
+  end
+end

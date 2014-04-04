6992fefbf3b1667f5eb9183dd82efd43ab0cd78f
danclark1820 <danclark1820@gmail.com>
2014-04-04 08:42:34 -0400

    create un authenicated user UX

---
+++ spec/features/un_auth_user_visit_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+feature " Un_authenticated user tries to rate movies", %q{
+  As an un-authenticated User
+  I want to be redirected to the sign in page
+  so I can sign in
+} do
+
+  #Acceptance Criteria
+  # * un-authenticated user can click on rate movies
+  # * will be redirected to sign in page
+
+  describe "enters site" do
+    it "rate list of movies" do
+      visit "/movies#index"
+      save_and_open_page
+      click_link 'Rate Movies'
+      expect(page).to have_content("Sign in")
+    end
+  end
+end

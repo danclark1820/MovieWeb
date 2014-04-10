6e8150f2eb5a6b45c4f00f137526b353165cffe2
danclark1820 <danclark1820@gmail.com>
2014-04-10 15:25:43 -0400

    Add validations and associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,6 @@ class Movie < ActiveRecord::Base
   validates :title, presence:true
   validates :year,  presence:true, format: {with: /\A\d{4}\z/, on: :create}
   validates :url,   presence:true, format: {with: /www.imdb.com/, on: :create}
+
+  has_many :ratings
 end

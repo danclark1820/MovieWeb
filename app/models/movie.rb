ad3985ff1e3b133cce7e32270655e7661fd53ac4
danclark1820 <danclark1820@gmail.com>
2014-04-10 15:33:06 -0400

    add destroy dependancies, has_many through associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,5 +3,6 @@ class Movie < ActiveRecord::Base
   validates :year,  presence:true, format: {with: /\A\d{4}\z/, on: :create}
   validates :url,   presence:true, format: {with: /www.imdb.com/, on: :create}
 
-  has_many :ratings
+  has_many :ratings, dependant: :destroy
+  has_many :raters, through: :ratings, source: :movies
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has many :ratings
+  has_many :ratings, dependant: :destroy
+  has_many :rated_movies, through: :ratings, source: :movies
 end

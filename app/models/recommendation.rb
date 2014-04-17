5d6062939b728c3c7344e19f1df213e388c70ae0
danclark1820 <danclark1820@gmail.com>
2014-04-17 10:04:56 -0400

    finish recommender, convert to module

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,6 +1,6 @@
-class Recommendation < ActiveRecord::Base
+module Recommendation
 
-  def self.sim_pearson(user_1, user_2)
+  def sim_pearson(user_1, user_2)
     #get list of mutually rated items
     mutually_rated={}
     user_1.ratings.pluck(:movie_id).each do |movie|
@@ -12,7 +12,7 @@ class Recommendation < ActiveRecord::Base
     n = mutually_rated.length
 
     if n==0
-      0
+      return 0
     end
 
     sum_1 = 0
@@ -31,47 +31,63 @@ class Recommendation < ActiveRecord::Base
 
 
     #calculte pearson
-    numerator = pSum - ( sum_1 * sum_2/n )
-    denominator=( sum_1Sq - (sum_1**2)/n ) * ( sum_2Sq - (sum_2**2)/n )**(1/2)
+    numerator = pSum - ( sum_1 * (sum_2/n).to_f )
+    denominator=( sum_1Sq - ((sum_1**2)/n).to_f )**(0.5) * ( sum_2Sq - ((sum_2**2)/n).to_f )**(0.5)
 
     if denominator == 0
-      0
+      r = 0
+    else
+     r = numerator/denominator
     end
 
-    r = numerator/denominator
+    return r
 
-    r
   end
 
 
-  # def getReccomendations(user_ratings_hash, user)
-  #   totals = {}
-  #   simSums= {}
-  #   user_ratings_hash.each do |person|
-  #     if person == user
-  #       continue
-  #     end
-  #     sim_pearson(user_ratings_hash, user, person)
-  #     if sim <= 0
-  #       continue
-  #     end
-  #     user_ratings_hash[person].each do |movie|
-  #       #only score movies I haven't seen yet
-  #       if movie not in user_ratings_hash[user] or user_ratings_hash[user][movie]==0
-  #         #Similarity * Score
-  #         totals.setdefault(item,0)
-  #         totals[item]+=user_ratings_hash[person][movie]*sim
-  #         #Sum of similarities
-  #         simSums.setdefault(item,0)
-  #         simSums[item]+=sim
-  #       end
+  def getReccomendations(user)
+    totals = {}
+    simSums={}
+    all_users = User.all
+    all_users.each do |person|
+      #don't compare users to themselves
+      if person == user
+        next
+      end
 
-  #     #Create normalized list
-  #     rankings=[(total/simSums[movie], movie)] for item, total in totals.item()]
+      sim = sim_pearson(user, person)
 
-  #     #Return sorted list
-  #     rankings.sort()
-  #     ranks.reverse()
-  #     rankings
+      if sim <= 0
+        next
+      end
+
+      person_ratings = User.where(id: person.id)[0].ratings
+      user_ratings = user.ratings
+
+      person_ratings.each do |rating|
+        #only score movies user has not seen
+        if !user_ratings.include?(rating)
+          #Similarity * Score
+          totals.default = 0
+          totals[rating.movie_id] += rating.stars * sim
+          #Sum of similarities
+          simSums.default = 0
+          simSums[rating.movie_id] += sim
+        end
+
+      end
+
+    end
+        #create the normalized list
+        rankings = {}
+        totals.each do |movie, sim_score_product|
+          simSums.each do |movie_simSum, sim|
+            rankings[movie] = sim_score_product/sim
+          end
+        end
+
+        rankings.sort_by{|k,v|}.reverse
+
+  end
 
 end

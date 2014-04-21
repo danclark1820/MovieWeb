8bba0dcb2f200d0d67065e1c00a36df2d1f57f08
danclark1820 <danclark1820@gmail.com>
2014-04-21 15:12:16 -0400

    switch to sql correlation over pearson

--- lib/recommendations.rb
+++ lib/recommendations.rb
@@ -1,46 +1,13 @@
 module Recommendations
 
   def sim_pearson(user_1, user_2)
-    #get list of mutually rated items
-    mutually_rated={}
-    user_1.ratings.pluck(:movie_id).each do |movie|
-      if user_2.ratings.pluck(:movie_id).include?(movie)
-        mutually_rated[movie] = 1
-      end
-    end
+    similarity = Rating.find_by_sql "select corr(u1.stars, u2.stars)
+      from ratings u1
+        inner join ratings u2
+          on u2.movie_id = u1.movie_id
+        where u2.user_id = #{user_2.id} and u1.user_id = #{user_1.id};"
 
-    n = mutually_rated.length
-
-    if n==0
-      return 0
-    end
-
-    sum_1 = 0
-    sum_2 = 0
-    sum_1Sq = 0
-    sum_2Sq = 0
-    pSum = 0
-
-    mutually_rated.each do |movie, value|
-      sum_1 += user_1.ratings.where(movie_id: movie).pluck(:stars)[0]
-      sum_1Sq += user_1.ratings.where(movie_id: movie).pluck(:stars)[0] ** 2
-      sum_2 += user_2.ratings.where(movie_id: movie).pluck(:stars)[0]
-      sum_2Sq += user_2.ratings.where(movie_id: movie).pluck(:stars)[0] ** 2
-      pSum += user_1.ratings.where(movie_id: movie).pluck(:stars)[0] * user_2.ratings.where(movie_id: movie).pluck(:stars)[0]
-    end
-
-
-    #calculte pearson
-    numerator = pSum - ( sum_1 * (sum_2/n).to_f )
-    denominator=( sum_1Sq - ((sum_1**2)/n).to_f )**(0.5) * ( sum_2Sq - ((sum_2**2)/n).to_f )**(0.5)
-
-    if denominator == 0
-      r = 0
-    else
-     r = numerator/denominator
-    end
-
-    return r
+    similarity[0].corr
 
   end
 
@@ -57,7 +24,7 @@ module Recommendations
 
       sim = sim_pearson(user, person)
 
-      if sim <= 0
+      if sim == nil || sim <= 0
         next
       end
 
@@ -85,8 +52,8 @@ module Recommendations
             rankings[movie] = sim_score_product/sim
           end
         end
-
-        rankings.sort_by{|k,v|}.reverse
+        binding.pry
+        rankings.sort_by{|k,v| v}.reverse
 
   end
 

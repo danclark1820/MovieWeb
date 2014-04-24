eee652b2cfe216825c11cfb13b1437894abf6118
danclark1820 <danclark1820@gmail.com>
2014-04-24 11:03:59 -0400

    Remove unused migrations and fix typo.

--- db/migrate/20140416154149_remove_index_on_url.rb
+++
@@ -1,4 +0,0 @@
-class RemoveIndexOnUrl < ActiveRecord::Migration
-  def change
-  end
-end
--- db/migrate/20140424120508_create_recommendations.rb
+++ db/migrate/20140424120508_create_recommendations.rb
@@ -1,6 +1,9 @@
 class CreateRecommendations < ActiveRecord::Migration
   def change
     create_table :recommendations do |t|
+      t.integer :user_id, null: false
+      t.integer :movie_id, null: false
+      t.integer :score, null: false
 
       t.timestamps
     end
